b0c1c15f4f3f33e23ec223cbdd0617ac7c000a76
py-py <valeev0506624980@gmail.com>
2018-10-11 18:29:57 +0300

    fix debug for swagger;

--- backend_rest/urls.py
+++ backend_rest/urls.py
@@ -1,4 +1,7 @@
 from rest_framework import routers
+from django.conf import settings
+from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
 
 from backend_rest.views import AddressViewSet, StreetTypeViewSet
 
@@ -8,12 +11,8 @@ router.register('streettype', StreetTypeViewSet)
 
 urlpatterns = router.urls
 
-
-from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Invoice API')
-
-urlpatterns += [
-    path('docs', schema_view)
-]
+if settings.DEBUG:
+    schema_view = get_swagger_view(title='Invoice API')
+    urlpatterns = [
+        path('docs', schema_view)
+    ]
